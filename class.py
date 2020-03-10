2627b58180181db215b78d2046a863ae503eb5b5
Залим Лампежев <sabolch@yandex.ru>
2020-03-10 12:00:26 +0300

    Update class.py
    Продолжаем движение. Классы - это здорово!

--- class.py
+++ class.py
@@ -46,10 +46,13 @@ restaurant_name и cuisine_type. Создайте метод describe_restaurant
 
 print("\n\n\n\t\t\t### <<< Здесь, значит, получается, начинается выполнение заданий 9.1 >>> ###")
 
+
 class Restaurant():
-    def __init__(self, restaurant_name, cuisine_type):
+    def __init__(self, restaurant_name, cuisine_type, number_served):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = number_served
+        self.number_served = 0
 
     def describe_restaurant(self):
         print("Ресторан, значит, называется " + "'" + self.restaurant_name.title() + "'." + " А тип ресторана: " +
@@ -59,7 +62,7 @@ class Restaurant():
         print("Значит так. Получается, ресторанчик наш, открыт. Так, получается?")
 
 
-restaurant = Restaurant("Del Mar", "Luxury")
+restaurant = Restaurant("Del Mar", "Luxury", 65)
 
 print("\n" + restaurant.restaurant_name)
 print(restaurant.cuisine_type + "\n")
@@ -67,12 +70,14 @@ print(restaurant.cuisine_type + "\n")
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
 
+print("Ресторан обслужил: " + str(restaurant.number_served) + " посетителей")
+
 """9.2. Три ресторана: начните с класса из упражнения 9.1. Создайте три разных экземпляра, вызовите для каждого 
 экземпляра метод describe_restaurant()."""
 
-fabrika = Restaurant("Fabrika", "lounge")
-pizzaHouse = Restaurant("pizza house", "Lux")
-indi = Restaurant("indi", "bistro")
+fabrika = Restaurant("Fabrika", "lounge", 12)
+pizzaHouse = Restaurant("pizza house", "Lux", 22)
+indi = Restaurant("indi", "bistro", 8)
 
 print("\n\n\n\t\t\t### <<< Здесь, значит, получается, начинается выполнение заданий 9.2 >>> ###")
 
@@ -88,24 +93,92 @@ indi.describe_restaurant()
 
 print("\n\n\n\t\t\t### <<< Здесь, значит, получается, начинается выполнение задания 9.2 >>> ###\n")
 
+
 class User():
-    def __init__(self, first_name, last_name, age, dick):
+    def __init__(self, first_name, last_name, age, dick, login_attempts):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.dick = dick
+        self.login_attempts = login_attempts
 
     def describe_user(self):
-        print("Имя: " + self.first_name.title() + "\nФамилия: " + self.last_name.title() + "\nВозраст: " + str(self.age) +
-              "\nЧлен: " + str(self.dick) + "см")
+        print(
+            "Имя: " + self.first_name.title() + "\nФамилия: " + self.last_name.title() + "\nВозраст: " + str(self.age) +
+            "\nЧлен: " + str(self.dick) + "см")
 
     def greet_user(self):
         print("Ассаламу Алейкум, " + self.first_name + "!")
 
+    def increment_login_attempts(self):
+        self.login_attempts += 1
 
-zalim = User("Залим", "Великолепный", 26, 15)
-vitalina = User("Виталик", "Давыдов", 27, 9)
-sabolch = User("Саболч", "Хусти", 35, 24)
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+
+zalim = User("Залим", "Великолепный", 26, 15, 0)
+vitalina = User("Виталик", "Давыдов", 27, 9, 0)
+sabolch = User("Саболч", "Хусти", 35, 24, 0)
 
 zalim.describe_user()
 vitalina.greet_user()
+
+zalim.increment_login_attempts()
+zalim.increment_login_attempts()
+zalim.increment_login_attempts()
+zalim.increment_login_attempts()
+zalim.increment_login_attempts()
+zalim.increment_login_attempts()
+
+print("Попыток входа: " + str(zalim.login_attempts))
+
+zalim.reset_login_attempts()
+print("<# reset login attempts #>".capitalize())
+print("Попыток входа: " + str(zalim.login_attempts))
+
+#
+class Car():
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 1000
+
+    def get_descriptive(self):
+        long_name = str(self.year) + " " + self.make.title() + " " + self.model.upper()
+        return long_name
+
+    def read_odometer(self):
+        print("Пробег этой машины составляет: " + str(self.odometer_reading) + " км.")
+
+    def update_odometer(self, mileage):
+        if mileage > self.odometer_reading:
+            self.odometer_reading += mileage
+        else:
+            print("ERROR! Нельзя сбрасывать счётчик пробега!")
+
+
+my_new_car = Car('mercedes', 'xxl', 2019)
+print("\n\n" + my_new_car.get_descriptive())
+my_new_car.update_odometer(2000)
+my_new_car.read_odometer()
+
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 90
+
+    def describe_battery(self):
+        print("Емкость аккумулятора: " + str(self.battery_size))
+
+    def read_odometer(self):
+        print("No odometer! This is Electric car")
+
+
+my_tesla = ElectricCar('tesla', 'model S', 2020)
+print(my_tesla.get_descriptive())
+my_tesla.describe_battery()
+my_tesla.read_odometer()
